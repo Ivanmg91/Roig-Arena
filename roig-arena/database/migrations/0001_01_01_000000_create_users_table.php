069bb594a25d342cbfa29f28775e177a33a022de
Iván Montiano González <160036159+Ivanmg91@users.noreply.github.com>
2026-03-17 09:35:09 +0000

    v.1.2.0 - Table migrations

--- roig-arena/database/migrations/0001_01_01_000000_create_users_table.php
+++ roig-arena/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('apellido'); // Added
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('is_admin')->default(false); // Added
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes(); // Added
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
